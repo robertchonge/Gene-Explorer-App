b706ed95a54cd7d3104088fadb23e1df703c6d08
Robert Chonge <106912093+robertchonge@users.noreply.github.com>
2025-08-02 22:38:43 +0200

    Create learn.svelte

---
+++ src/routes/learn.svelte
@@ -0,0 +1,8 @@
+<script>
+  import ProteinAnimator from '../components/ProteinAnimator.svelte';
+</script>
+
+<main class="p-6">
+  <h1 class="text-2xl font-bold underline mb-4">Learn About Protein Synthesis</h1>
+  <ProteinAnimator />
+</main>
