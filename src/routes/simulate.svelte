06ea985377f473d02fc626df5a702b9f6da3691c
Robert Chonge <106912093+robertchonge@users.noreply.github.com>
2025-08-02 22:39:18 +0200

    Create simulate.svelte

---
+++ src/routes/simulate.svelte
@@ -0,0 +1,8 @@
+script>
+  import MutationSimulator from '../components/MutationSimulator.svelte';
+</script>
+
+<main class="p-6">
+  <h1 class="text-2xl font-bold underline mb-4">Mutation Simulator</h1>
+  <MutationSimulator />
+</main>
